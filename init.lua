32f72953ab2642ae9c06733a326fde5e69183fe5
Sudhir Krisna <sudhboiii@gmail.com>
2026-02-24 12:47:30 -0400

    Tue 24 Feb 2026 12:47:30 PM AST - Sync Commit

--- init.lua
+++ init.lua
@@ -3,19 +3,19 @@ require("config.lazy")
 
 require("luasnip").config.set_config({ -- Setting LuaSnip config
 
-  -- Enable autotriggered snippets
-  enable_autosnippets = true,
+    -- Enable autotriggered snippets
+    enable_autosnippets = true,
 
-  -- Use Tab (or some other key if you prefer) to trigger visual selection
-  --store_selection_keys = "<Tab>",
+    -- Use Tab (or some other key if you prefer) to trigger visual selection
+    --store_selection_keys = "<Tab>",
 
-  update_events = "TextChanged,TextChangedI",
-  link_roots = false,
+    update_events = "TextChanged,TextChangedI",
+    link_roots = false,
 })
 
 vim.cmd([[
     " Expand or jump in insert mode
-    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+    imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
 
     " Jump forward through tabstops in visual mode
     smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
@@ -57,3 +57,9 @@ au BufReadPre *.lagda* call CornelisLoadWrapper()
 
 au BufWritePost *.agda execute "normal! :CornelisLoad\<CR>"
 ]])
+
+require 'nvim-treesitter.config'.setup {
+    indent = {
+        enable = true
+    },
+}
