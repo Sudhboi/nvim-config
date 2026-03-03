f42904cbaaadb884ed74afb0d20f705a6207f548
Sudhir Krisna <sudhboiii@gmail.com>
2026-03-03 00:32:41 -0400

    Tue 03 Mar 2026 12:32:41 AM AST - Sync Commit

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
         "saghen/blink.cmp",
         opts = {
             enabled = function()
-                return vim.tbl_contains({ "tex", "lean", "lua", "typst" }, vim.bo.filetype)
+                return vim.tbl_contains({ "tex", "lean", "lua", "typst", "nix" }, vim.bo.filetype)
             end,
         },
     },
